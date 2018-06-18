1e0f190aa76df6017c6295deff8430b6da7c83a9
nithin <nithincv1@gmail.com>
2018-06-18 23:30:16 +1000

    componnents remvd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,10 @@ import { NavmenuModule } from './navmenu/navmenu.module';
 import { HomeModule } from './home/home.module';
 import { SettingsModule } from './settings/settings.module';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { VersionComponent } from './version/version.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    VersionComponent,
-    PagenotfoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule, RoutingModule, NoopAnimationsModule, NavmenuModule, HomeModule, SettingsModule
